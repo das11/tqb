b531a034bf9731a64a72cb5e69abae263004b1b0
Kabir Das <kabird1196@gmail.com>
2021-05-28 20:07:23 +0530

    Update waye.js

--- js/waye.js
+++ js/waye.js
@@ -214,7 +214,7 @@ $(function() {
     // 9.2. countdown SETUP
     var setting = {
         counter: {
-            lastDate: "06/05/2021 12:00:00", // target date settings, 'MM/DD/YYYY HH:MM:SS'
+            lastDate: "06/30/2021 12:00:00", // target date settings, 'MM/DD/YYYY HH:MM:SS'
             timeZone: null
         }
     };
